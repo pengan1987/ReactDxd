61de115e97859b3d2c4825b32462b018878897e5
Andy Zhou <andy.zhou@fandine.com>
2017-03-25 14:53:21 -0700

    show student details

--- client/app.jsx
+++ client/app.jsx
@@ -1,6 +1,39 @@
 var Student = React.createClass({
+    getInitialState: function () {
+        return {
+            firstName: null,
+            lastName: null,
+            age: null
+        }
+    },
+    handleClick: function () {
+        console.log("click");
+        var reactThis = this;
+        axios.get('http://localhost:3000/student/' + this.props.data._id)
+            .then(function (response) {
+                console.log(response.data);
+                reactThis.setState(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    },
     render: function () {
-        return <h2>{this.props.data.firstName}</h2>
+        console.log(this.props);
+        var nameNode = <h2 onClick={this.handleClick}>{this.props.data.firstName}</h2>;
+        var fullNode = (
+            <div>
+                <p>First Name: {this.state.firstName}</p>
+                <p>Last Name: {this.state.lastName}</p>
+                <p>Age: {this.state.age}</p>
+                <p>Email: {this.state.email}</p>
+                <hr />
+            </div>
+        );
+        if (this.state.firstName != null)
+            return fullNode;
+        else
+            return nameNode;
     }
 });
 
@@ -21,7 +54,12 @@ var StudentList = React.createClass({
                 console.log(error);
             });
     },
+
+    handleClick: function () {
+        console.log("Click");
+    },
     render: function () {
+
         return (<div>
             {
                 this.state
